bb283dd99798248aa25bc53116847b895db9d5ea
Lovish Bansal <lovishbansal441@gmail.com>
2024-07-24 06:41:46 +0530

    Basic Frontend Setup Backend - added .env.sample

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+    return (
+        <div className="h-[100vh] w-full bg-black text-white flex justify-center items-center">
+            <h1 className="text-4xl">Hello World!</h1>
+        </div>
+    );
+};
